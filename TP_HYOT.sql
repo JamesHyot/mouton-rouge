a756808bda948763e99291f9d022815600199c19
JamesHyot <james.hyot@gmail.com>
2015-12-15 14:23:38 +0100

    creation de tables moto et accessoires

--- TP_HYOT.sql
+++ TP_HYOT.sql
@@ -1,21 +1,37 @@
-DROP TABLE TT1_personnage_type
+DROP TABLE moto
+/
+DROP TYPE moto_type
+/
+DROP TYPE accessoire_ntab_type
+/
+DROP TYPE accessoire_type
 /
 
-CREATE TYPE TT1_personnage_type AS OBJECT(
-id INTEGER,
-nom VARCHAR2(20),
-prenom VARCHAR2(20),
-profession VARCHAR2(20),
-sexe CHAR,
-nature VARCHAR2(7),
-nom_etranger TT2_nom_etranger_ntab_type
+CREATE TYPE accessoire_type AS OBJECT (
+  nom VARCHAR2(40),
+  marque VARCHAR2(40),
+  prix FLOAT
+)
+/
+
+CREATE OR REPLACE TYPE accessoire_ntab_type AS TABLE OF accessoire_type
+/
+
+CREATE TYPE moto_type AS OBJECT(
+immatriculation VARCHAR2(20),
+numSerie INTEGER,
+modele VARCHAR2(40),
+marque VARCHAR2(40),
+cylindree INTEGER,
+prixCat FLOAT,
+type VARCHAR2(20),
+pourcentageCr FLOAT,
+accessoire accessoire_ntab_type
 );
 /
 
-CREATE TABLE TT1_personnage OF TT1_personnage_type
+CREATE TABLE moto OF moto_type
 (
-	CONSTRAINT pk_id PRIMARY KEY(id),
-	CONSTRAINT nature_in CHECK(nature = 'GENTIL' OR nature = 'MECHANT'),
-	CONSTRAINT sexe_in CHECK(sexe = 'M' OR sexe = 'F')
-) NESTED TABLE nom_etranger STORE AS tab_perso_etran
+	CONSTRAINT pk_immat PRIMARY KEY(immatriculation)
+) NESTED TABLE accessoire STORE AS tab_access
 /
